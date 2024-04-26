295d0424bbe7d4e462ba05fec7b32cef9229832a
Natacha Rivière <riviere.natachamarie@gmail.com>
2024-04-26 19:30:17 +0200

    hope this is the end

--- dedale-etu/src/main/java/eu/su/mas/dedaleEtu/princ/ConfigurationFile.java
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/princ/ConfigurationFile.java
@@ -77,13 +77,13 @@ public final class ConfigurationFile {
 	
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2020-topologyExam1-graph.dgs";
 	//public static String INSTANCE_TOPOLOGY="resources/topology/HouatTopology";
-//	public static String INSTANCE_TOPOLOGY="resources/topology/map2018-topology-ica";
+	public static String INSTANCE_TOPOLOGY="resources/topology/map2018-topology-ica";
 //	public static String INSTANCE_TOPOLOGY="resources/topology/map2021-topologyExam1-tree.dgs";
 //	public static String INSTANCE_TOPOLOGY="resources/topology/mapInterlocking2-topology";
 //	public static String INSTANCE_TOPOLOGY="resources/topology/map2023-topologyExam.dgs";
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2020-topologyExam1-graph.dgs";
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2018-topology";
-	public static String INSTANCE_TOPOLOGY="resources/topology/map2022-topologyExam1-draft.dgs";
+//	public static String INSTANCE_TOPOLOGY="resources/topology/map2022-topologyExam1-draft.dgs";
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2022-topologyExam-V2b.dgs";
 //	public static String INSTANCE_TOPOLOGY="resources/topology/map2019-topologyExam1";
 	
